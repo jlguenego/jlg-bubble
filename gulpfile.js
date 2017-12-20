92011bf577aa1bf7d91a292bcbc240ce9e3b56ce
Maite THOMIAS <maite.thomias@gmail.com>
2017-12-20 08:58:47 +0100

    add: babel gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,26 @@
 const gulp = require('gulp');
 const ghPages = require('gulp-gh-pages');
+const babel = require('gulp-babel');
+const del = require('del');
+
+gulp.task('clean', function () {
+    return del(['dist', '.publish']);
+});
 
 gulp.task('deploy', function () {
     return gulp.src('./example/**/*')
         .pipe(ghPages());
 });
+
+gulp.task('build:js', function () {
+    return gulp.src('src/**/*.js')
+        .pipe(babel())
+        .pipe(gulp.dest('dist'));
+});
+
+gulp.task('build:css', function () {
+    return gulp.src('src/**/*.css')
+        .pipe(gulp.dest('dist'));
+});
+
+gulp.task('build', ['build:js', 'build:css']);
